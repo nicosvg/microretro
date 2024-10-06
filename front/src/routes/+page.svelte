f7086f072203ab399c813e4a540283a371cce297
Nicolas Sauvage <nicolas.sauvage@renaissance.com>
2024-10-06 15:18:22 +0200

    init svelte frontend

---
+++ front/src/routes/+page.svelte
@@ -0,0 +1,51 @@
+<script>
+	async function createRetrospective() {
+		console.log('Creating retrospective');
+		const response = await fetch('http://localhost:3000/boards', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+		if (response.ok) {
+			alert('Retrospective created');
+		} else {
+			alert('Failed to create retrospective');
+		}
+	}
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+		<span class="welcome"> MICRORETRO </span>
+	</h1>
+
+	<button on:click={createRetrospective}>Create retrospective</button>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+</style>
