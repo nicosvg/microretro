fdee29c10c22380176ae7ea676779063e57f612a
Nicolas Sauvage <nicolas.sauvage@renaissance.com>
2024-10-09 22:33:52 +0200

    add tailwind

--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script>
+	import '../app.css';
 	// import Header from './Header.svelte';
 	import '../app.css';
 </script>
@@ -7,7 +8,7 @@
 	<!-- <Header /> -->
 
 	<main>
-		<slot />
+		<slot></slot>
 	</main>
 
 	<footer>
