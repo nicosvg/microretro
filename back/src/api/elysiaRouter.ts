01b3b8dd3a834eaf7aa5cf180fe6d8e5ec6615f9
Nicolas Sauvage <nicolas.sauvage@renaissance.com>
2024-10-15 23:51:20 +0200

    replace Hono with Elysia

---
+++ back/src/api/elysiaRouter.ts
@@ -0,0 +1,53 @@
+import { createBoard } from "../core/usecases/createBoard";
+import { type BoardRepository } from "../core/ports/BoardRepository";
+import { getBoard } from "../core/usecases/getBoard";
+import type { UserRepository } from "../core/ports/UserRepository";
+import { createCard } from "../core/usecases/createCard";
+import type { CardRepository } from "../core/ports/CardRepository";
+import { createUser } from "../core/usecases/createUser";
+import { PubSubEvent, type PubSubGateway } from "../core/ports/PubSubGateway";
+import Elysia, { t } from "elysia";
+import type { User } from "../core/domain/user";
+import { cors } from '@elysiajs/cors'
+
+const mockUserId = '5ab0aebc-6e82-4ecb-9066-061153a5ddae'
+
+export function initElysiaRouter(boardRepo: BoardRepository, userRepo: UserRepository, cardRepo: CardRepository, pubSub: PubSubGateway) {
+  new Elysia()
+    .use(cors())
+    .get('/', 'Hello Elysia!')
+    .post('/boards', async () => {
+      const id = await createBoard(boardRepo)()
+      return { id: id };
+    })
+    .get('/boards/:id', async ({ params: { id } }) => {
+      const boardId = id
+      if (!boardId) { throw new Error('boardId is required') }
+      console.log(boardId)
+      const board = await getBoard(boardId, boardRepo)
+      return board;
+    })
+    .post('/boards/:boardId/cards', async ({ body, params: { boardId } }) => {
+      const text = body.text
+      if (boardId === undefined) {
+        throw new Error('boardId is required')
+      }
+      const cardId = await createCard(boardId, mockUserId, text, cardRepo)
+      console.log('Created a new card', cardId)
+      pubSub.publish(boardId, { event: PubSubEvent.CREATED_CARD, payload: { id: cardId, text } })
+      return { cardId: cardId };
+    }, { body: t.Object({ text: t.String() }) })
+    .post('/users', async ({ body }) => {
+      const userData = body
+      const id = await createUser(userRepo)(userData as User)
+      return { id: id };
+    }, { body: t.Any() })
+    .ws('/ws', {
+      message(ws, message) {
+        ws.send(message)
+      }
+    })
+    .listen(3000);
+}
+
+
--- back/src/index.ts
+++ back/src/index.ts
@@ -5,6 +5,8 @@ import { DrizzleBoardRepo } from "./persistance/drizzle/DrizzleBoardRepo";
 import { DrizzleUserRepo } from "./persistance/drizzle/DrizzleUserRepo";
 import { DrizzleCardRepo } from "./persistance/drizzle/DrizzleCardRepo";
 import type { PubSubGateway } from "./core/ports/PubSubGateway";
+import cookieParser from 'cookie-parser'
+import { initElysiaRouter } from "./api/elysiaRouter";
 
 console.log("Initialize DB");
 const drizzleDB: NodePgDatabase = await getDrizzleDB();
@@ -15,47 +17,49 @@ const userRepo = new DrizzleUserRepo(drizzleDB)
 const cardRepo = new DrizzleCardRepo(drizzleDB)
 
 console.log('Initialize websocket server')
-export const wsServer = Bun.serve<{ username: string }>({
-  fetch(req, server) {
-    const url = new URL(req.url);
-    if (url.pathname === "/ws") {
-      console.log(`upgrade!`);
-      // const username = getUsernameFromReq(req);
-      const success = server.upgrade(req, { data: 'coucou' });
-      return success
-        ? undefined
-        : new Response("WebSocket upgrade error", { status: 400 });
-    }
-
-    return new Response("Hello world");
-  },
-  websocket: {
-    open(ws) {
-      wsServer.publish("board_updates", JSON.stringify({ event: 'JOINED_BOARD', message: 'coucou' }))
-      ws.subscribe("board_updates");
-    },
-    message(ws, message) {
-      console.log(`Received message: ${message}`);
-    },
-    close(ws) {
-      ws.unsubscribe("board_updates");
-    },
-  },
-  port: 3001,
-});
+// export const wsServer = Bun.serve<{ username: string }>({
+//   fetch(req, server) {
+//     const url = new URL(req.url);
+//     if (url.pathname === "/ws") {
+//       console.log(`upgrade!`);
+//       // const username = getUsernameFromReq(req);
+//       // get cookie from request
+//       const cookie = req.headers.get('Cookie')
+//       console.log('cookie', cookie)
+//       if (!cookie) {
+//         return;
+//       }
+//       const res = cookieParser.JSONCookie(cookie)
+//       console.log('res', res)
+//       const success = server.upgrade(req, { data: 'coucou' });
+//       return success
+//         ? undefined
+//         : new Response("WebSocket upgrade error", { status: 400 });
+//     }
+//
+//     return new Response("Hello world");
+//   },
+//   websocket: {
+//     open(ws) {
+//       wsServer.publish("board_updates", JSON.stringify({ event: 'JOINED_BOARD', message: 'coucou' }))
+//       ws.subscribe("board_updates");
+//     },
+//     message(ws, message) {
+//       console.log(`Received message: ${message}`);
+//     },
+//     close(ws) {
+//       ws.unsubscribe("board_updates");
+//     },
+//   },
+//   port: 3001,
+// });
 
 const pubSub: PubSubGateway = {
   //TODO: update with correct channel: boardId
-  publish: (channel, message) => wsServer.publish("board_updates", JSON.stringify(message))
+  // publish: (_, message) => wsServer.publish("board_updates", JSON.stringify(message))
 }
 
 console.log("Initialize router");
-const app = initRouter(boardRepo, userRepo, cardRepo, pubSub)
+initElysiaRouter(boardRepo, userRepo, cardRepo, pubSub)
 
 console.log("Hello from Bun!!!");
-
-Bun.serve({
-  fetch: app.fetch,
-  port: 3000,
-});
-
