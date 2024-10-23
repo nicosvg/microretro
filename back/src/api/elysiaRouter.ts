15f02f73b786d23b0a3a048ac43689472fbb39d1
Nicolas Sauvage <nicolas.sauvage@renaissance.com>
2024-10-23 21:55:05 +0200

    formatting

--- back/src/api/elysiaRouter.ts
+++ back/src/api/elysiaRouter.ts
@@ -8,121 +8,177 @@ import { createUser } from "../core/usecases/createUser";
 import { PubSubEvent, type PubSubGateway } from "../core/ports/PubSubGateway";
 import Elysia, { t } from "elysia";
 import type { User } from "../core/domain/user";
-import { cors } from '@elysiajs/cors'
-import { jwt } from '@elysiajs/jwt'
+import { cors } from "@elysiajs/cors";
+import { jwt } from "@elysiajs/jwt";
 import bearer from "@elysiajs/bearer";
 import { joinBoard } from "../core/usecases/joinBoard";
 import { getUser } from "../core/usecases/getUser";
 
 interface UserProfile {
-  id: string
-  name: string
+  id: string;
+  name: string;
 }
 
 const jwtValidator = jwt({
-  name: 'jwt',
-  secret: '5ab0aebc-6e82-4ecb-9065-061153a5ddae'
-})
+  name: "jwt",
+  secret: "5ab0aebc-6e82-4ecb-9065-061153a5ddae",
+});
 
-export function initElysiaRouter(boardRepo: BoardRepository, userRepo: UserRepository, cardRepo: CardRepository, pubSub: PubSubGateway) {
+export function initElysiaRouter(
+  boardRepo: BoardRepository,
+  userRepo: UserRepository,
+  cardRepo: CardRepository,
+  pubSub: PubSubGateway,
+) {
   new Elysia()
     .use(cors())
     .use(bearer())
     .use(jwtValidator)
-    .get('/', 'Hello Elysia!')
+    .get("/", "Hello Elysia!")
 
-    .post('/boards', async ({ jwt, set, bearer }) => {
-      const profile = await jwt.verify(bearer) as UserProfile | false
+    .post("/boards", async ({ jwt, set, bearer }) => {
+      const profile = (await jwt.verify(bearer)) as UserProfile | false;
       if (!profile) {
-        set.status = 401
-        return 'Unauthorized'
+        set.status = 401;
+        return "Unauthorized";
       }
 
-      const id = await createBoard(boardRepo)(profile.id)
+      const id = await createBoard(boardRepo)(profile.id);
       return { id: id };
     })
 
-    .post('/boards/:boardId/join', async ({ params: { boardId }, jwt, set, bearer }) => {
-      const profile = await jwt.verify(bearer) as UserProfile | false
+    .post(
+      "/boards/:boardId/join",
+      async ({ params: { boardId }, jwt, set, bearer }) => {
+        const profile = (await jwt.verify(bearer)) as UserProfile | false;
+        if (!profile) {
+          set.status = 401;
+          return "Unauthorized";
+        }
+
+        await joinBoard(boardRepo)(boardId, profile.id);
+        const user = await getUser(userRepo)(profile.id);
+
+        pubSub.publish(boardId, {
+          event: PubSubEvent.JOINED_BOARD,
+          payload: { user: user },
+        });
+      },
+    )
+
+    .get("/boards/:id", async ({ params: { id }, jwt, set, bearer }) => {
+      const profile = (await jwt.verify(bearer)) as UserProfile | false;
       if (!profile) {
-        set.status = 401
-        return 'Unauthorized'
+        set.status = 401;
+        return "Unauthorized";
       }
 
-      await joinBoard(boardRepo)(boardId, profile.id)
-      const user = await getUser(userRepo)(profile.id)
-
-      pubSub.publish(boardId, { event: PubSubEvent.JOINED_BOARD, payload: { user: user } })
-    })
-
-    .get('/boards/:id', async ({ params: { id }, jwt, set, bearer }) => {
-      const profile = await jwt.verify(bearer) as UserProfile | false
-      if (!profile) {
-        set.status = 401
-        return 'Unauthorized'
+      const boardId = id;
+      if (!boardId) {
+        throw new Error("boardId is required");
       }
-
-      const boardId = id
-      if (!boardId) { throw new Error('boardId is required') }
-      const board = await getBoard(boardId, boardRepo)
+      const board = await getBoard(boardId, boardRepo);
       return board;
     })
 
-    .post('/boards/:boardId/cards', async ({ body, params: { boardId }, set, jwt, bearer }) => {
-      const profile = await jwt.verify(bearer) as UserProfile | false
-      if (!profile) {
-        set.status = 401
-        return 'Unauthorized'
-      }
+    .post(
+      "/boards/:boardId/cards",
+      async ({ body, params: { boardId }, set, jwt, bearer }) => {
+        const profile = (await jwt.verify(bearer)) as UserProfile | false;
+        if (!profile) {
+          set.status = 401;
+          return "Unauthorized";
+        }
 
-      const text = body.text
-      if (boardId === undefined) {
-        throw new Error('boardId is required')
-      }
-      const card = await createCard(boardId, profile.id, text, body.column, cardRepo)
-      console.log('Created a new card', card)
-      pubSub.publish(boardId, { event: PubSubEvent.CREATED_CARD, payload: { card } })
+        const text = body.text;
+        if (boardId === undefined) {
+          throw new Error("boardId is required");
+        }
+        const card = await createCard(
+          boardId,
+          profile.id,
+          text,
+          body.column,
+          cardRepo,
+        );
+        console.log("Created a new card", card);
+        pubSub.publish(boardId, {
+          event: PubSubEvent.CREATED_CARD,
+          payload: { card },
+        });
 
-      return { card: card };
-    }, { body: t.Object({ text: t.String(), column: t.Number() }) })
+        return { card: card };
+      },
+      { body: t.Object({ text: t.String(), column: t.Number() }) },
+    )
 
-    .post('/users', async ({ body, jwt }) => {
-      const userData = body
-      const createdUserId = await createUser(userRepo)(userData as User)
-      const token = await jwt.sign({ name: userData.name, id: createdUserId })
-      return { id: createdUserId, token };
-    }, {
-      body: t.Object({ name: t.String() }),
-    })
+    .post(
+      "/boards/:boardId/nextState",
+      async ({ params: { boardId }, set, jwt, bearer }) => {
+        const profile = (await jwt.verify(bearer)) as UserProfile | false;
+        if (!profile) {
+          set.status = 401;
+          return "Unauthorized";
+        }
+
+        if (boardId === undefined) {
+          throw new Error("boardId is required");
+        }
+        // const card = await createCard(boardId, profile.id, text, body.column, cardRepo)
+        // console.log('Created a new card', card)
+        // pubSub.publish(boardId, { event: PubSubEvent.CREATED_CARD, payload: { card } })
+
+        return {};
+      },
+      { body: t.Object({ text: t.String(), column: t.Number() }) },
+    )
+
+    .post(
+      "/users",
+      async ({ body, jwt }) => {
+        const userData = body;
+        const createdUserId = await createUser(userRepo)(userData as User);
+        const token = await jwt.sign({
+          name: userData.name,
+          id: createdUserId,
+        });
+        return { id: createdUserId, token };
+      },
+      {
+        body: t.Object({ name: t.String() }),
+      },
+    )
 
     .listen({ port: 3000 });
 
   new Elysia()
     .use(jwtValidator)
-    .ws('/ws/:boardId', {
+    .ws("/ws/:boardId", {
       async beforeHandle({ jwt, query: { access_token } }) {
-        const res = await jwt.verify(access_token)
+        const res = await jwt.verify(access_token);
         // __AUTO_GENERATED_PRINT_VAR_START__
         console.log("initElysiaRouter#beforeHandle res ", res); // __AUTO_GENERATED_PRINT_VAR_END__
         if (!res) {
-          throw new Error('Unauthorized')
+          throw new Error("Unauthorized");
         }
       },
       message(ws, message) {
-        ws.send(message)
-        console.log('received message', message)
+        ws.send(message);
+        console.log("received message", message);
       },
       open(ws) {
-        const { boardId } = ws.data.params
-        console.log('open socket')
-        ws.send(JSON.stringify('Hello from server!, you are subscribed to board ' + boardId));
+        const { boardId } = ws.data.params;
+        console.log("open socket");
+        ws.send(
+          JSON.stringify(
+            "Hello from server!, you are subscribed to board " + boardId,
+          ),
+        );
         pubSub.subscribe(boardId, (data: any) => {
-          console.debug('Sending message for board ' + boardId, data)
+          console.debug("Sending message for board " + boardId, data);
           ws.send(JSON.stringify(data));
-        })
+        });
       },
     })
-    .listen(3001)
+    .listen(3001);
 }
-
-
--- work-log.md
+++ work-log.md
@@ -6,7 +6,7 @@
 | Sun 6 Oct  | 1h30     | Init DB and Drizzle                  |
 | Mon 7 Oct  | 30min    | Create a board                       |
 | Tue 8 Oct  | 1h30     | Create first cards                   |
-| Wed 9 Oct  | 45 min   | Add Skeleton UI                      |
+| Wed 9 Oct  | 1h       | Add Skeleton UI                      |
 | Thu 10 Oct | 30 min   | Add join table for user <-> board    |
 | Sat 12 Oct | 1h30 min | Cleanups: errors, board with details |
 | Sun 13 Oct | 30 min   | Websockets                           |
