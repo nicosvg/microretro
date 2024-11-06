172c36b360c077d80777d38fcc99fcc5ed17c2a6
Nicolas Sauvage <nicolas.sauvage@renaissance.com>
2024-11-06 20:20:39 +0100

    fix drizzle config
    fix disconnect from server

--- back/src/persistance/drizzle/drizzle.config.ts
+++ back/src/persistance/drizzle/drizzle.config.ts
@@ -2,33 +2,37 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Client } from "pg";
 import { defineConfig } from "drizzle-kit";
 
+const DB_HOST = "116.203.211.97";
+
 export default defineConfig({
   dialect: "postgresql",
   schema: "./src/persistance/drizzle/schema.ts",
   out: "src/persistance/drizzle/migrations",
   dbCredentials: {
-    host: "116.203.211.97",
+    host: DB_HOST,
     port: 5432,
     user: "postgres",
     password: process.env.DB_PASSWORD,
     database: "microretro",
-    ssl: false
-  }
+    ssl: false,
+  },
 });
 
 async function initDrizzleClient() {
   const client = new Client({
-    host: "116.203.211.97",
+    host: DB_HOST,
     port: 5432,
     user: "postgres",
     password: process.env.DB_PASSWORD,
     database: "microretro",
+    keepAlive: true,
   });
   await client.connect();
   return client;
 }
 
 export async function getDrizzleDB() {
-  const client = await initDrizzleClient()
-  return drizzle(client, { logger: true })
+  // const client = await initDrizzleClient();
+  const connectionString = `postgresql://postgres:${process.env.DB_PASSWORD}@${DB_HOST}:5432/microretro`;
+  return drizzle(connectionString, { logger: true });
 }
