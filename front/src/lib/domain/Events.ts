e08b3f0861a36fe1a71390ee4f62f48044c6a614
Nicolas Sauvage <nicolas.sauvage@renaissance.com>
2025-01-26 22:45:44 +0100

    delete old domain files

--- front/src/lib/domain/Events.ts
+++
@@ -1,9 +0,0 @@
-export const Events = {
-	CREATED_CARD: 'CREATED_CARD',
-	JOINED_BOARD: 'JOINED_BOARD',
-	CONNECTED: 'CONNECTED'
-} as const;
-export type MessageData = {
-	event: (typeof Events)[keyof typeof Events];
-	payload: unknown;
-};
