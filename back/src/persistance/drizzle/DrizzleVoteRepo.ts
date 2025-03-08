23aed55c33fdeaa6deb6f40ea36b14c475acee8c
Nicolas Sauvage <nicolas.sauvage@renaissance.com>
2025-03-08 15:51:50 +0100

    add migration

--- back/src/persistance/drizzle/DrizzleVoteRepo.ts
+++ back/src/persistance/drizzle/DrizzleVoteRepo.ts
@@ -1,9 +1,9 @@
-import type { CardId } from '@domain/card';
-import type { GroupId } from '@domain/group';
-import type { UserId } from '@domain/user';
-import type { NodePgDatabase } from 'drizzle-orm/node-postgres';
-import type { VoteRepository } from '../../core/ports/VoteRepository';
-import { votes } from './schema';
+import type { CardId } from "@domain/card";
+import type { GroupId } from "@domain/group";
+import type { UserId } from "@domain/user";
+import type { NodePgDatabase } from "drizzle-orm/node-postgres";
+import type { VoteRepository } from "../../core/ports/VoteRepository";
+import { votes } from "./schema";
 
 export class DrizzleVoteRepo implements VoteRepository {
   constructor(private db: NodePgDatabase) {}
