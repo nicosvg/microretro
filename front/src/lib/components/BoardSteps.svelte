e32765c1a6f99f6d3bd5638663d6246d2837e8f3
Nicolas Sauvage <nicolas.sauvage@renaissance.com>
2025-06-18 23:48:17 +0200

    show steps workflow

--- front/src/lib/components/BoardSteps.svelte
+++ front/src/lib/components/BoardSteps.svelte
@@ -30,13 +30,43 @@
 		[BoardStep.DISCUSS]: { index: 4, label: 'Discuss' },
 		[BoardStep.DONE]: { index: 5, label: 'Done!' }
 	};
+
+	const currentStepIndex = $derived(steps[boardStep].index);
 </script>
 
-<section class="card variant-soft-surface flex items-center justify-between p-4" id="steps">
-	<h2 class="h3 text-tertiary-500">Step {steps[boardStep].index}/4</h2>
-	<div class="flex flex-col">
-		<h2 class="h3 text-tertiary-500">{steps[boardStep].label}</h2>
-		<p class="text-tertiary-400 w-96 text-sm">
+<section class="card variant-soft-surface flex w-full flex-col gap-4 p-4" id="steps">
+	<fragment class="flex w-full items-center">
+		{#each Object.entries(steps) as [step, { index, label }]}
+			<div class="flex items-center">
+				<div
+					class="btn-icon variant-filled flex h-8 w-8 items-center justify-center {currentStepIndex ===
+					index
+						? 'variant-filled-primary'
+						: currentStepIndex > index
+							? 'variant-filled-surface'
+							: 'variant-filled-secondary'}"
+				>
+					{index}
+				</div>
+				<div
+					class="text-md ml-2 {currentStepIndex === index
+						? 'text-primary-300 font-semibold'
+						: currentStepIndex > index
+							? 'text-blue-300'
+							: 'text-cyan-300'}"
+				>
+					{label}
+				</div>
+			</div>
+			{#if index < Object.keys(steps).length}
+				<div
+					class="mx-2 h-0.5 grow {currentStepIndex > index ? 'bg-blue-100' : 'bg-blue-100'}"
+				></div>
+			{/if}
+		{/each}
+	</fragment>
+	<div class="flex w-full items-center justify-between gap-4">
+		<p class="text-tertiary-400 text-md w-96 grow">
 			{#if boardStep === BoardStep.WRITE}
 				Write down your thoughts in each column. Your cards are private and will only be revealed
 				during the next step.
@@ -50,24 +80,24 @@
 				Review the retrospective outcomes
 			{/if}
 		</p>
-	</div>
-	<div class="flex items-center gap-2">
-		{#if (boardStep === BoardStep.WRITE || boardStep === BoardStep.VOTE) && !readyUsers.includes(connectedUserId)}
-			<button class="variant-filled-surface btn" onclick={() => onReadyClick()}>
-				{readyUsers.includes(connectedUserId) ? 'Not ready' : `I'm ready!`}
+		<div class="flex items-center gap-2">
+			{#if (boardStep === BoardStep.WRITE || boardStep === BoardStep.VOTE) && !readyUsers.includes(connectedUserId)}
+				<button class="variant-filled-surface btn" onclick={() => onReadyClick()}>
+					{readyUsers.includes(connectedUserId) ? 'Not ready' : `I'm ready!`}
+				</button>
+			{/if}
+			<button
+				disabled={boardStep === BoardStep.DONE}
+				class="{allUsersAreReady ? 'variant-filled-success' : 'variant-filled-surface'} btn"
+				onclick={() => onNextStep()}>Next step</button
+			>
+			<button
+				disabled={boardStep === BoardStep.WRITE}
+				class="variant-ghost-surface btn btn-icon"
+				onclick={() => onPreviousStep()}
+			>
+				<Undo2 />
 			</button>
-		{/if}
-		<button
-			disabled={boardStep === BoardStep.DISCUSS}
-			class="{allUsersAreReady ? 'variant-filled-success' : 'variant-filled-surface'} btn"
-			onclick={() => onNextStep()}>Next step</button
-		>
-		<button
-			disabled={boardStep === BoardStep.WRITE}
-			class="variant-ghost-surface btn btn-icon"
-			onclick={() => onPreviousStep()}
-		>
-			<Undo2 />
-		</button>
+		</div>
 	</div>
 </section>
