a8be0d044127aee34f5b569e5958cdaa5fb831ca
Nicolas Sauvage <nicolas.sauvage@renaissance.com>
2025-07-02 10:15:44 +0200

    prepare tailwind migration

--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -1,8 +1,6 @@
 import { join } from 'path';
 import type { Config } from 'tailwindcss';
 
-// 1. Import the Skeleton plugin
-import { skeleton } from '@skeletonlabs/tw-plugin';
 
 const config = {
 	// 2. Opt for dark mode to be handled via the class method
@@ -15,12 +13,7 @@ const config = {
 	theme: {
 		extend: {}
 	},
-	plugins: [
-		// 4. Append the Skeleton plugin (after other plugins)
-		skeleton({
-			themes: { preset: [{ name: 'modern', enhancements: true }] }
-		})
-	]
+	plugins: []
 } satisfies Config;
 
 export default config;
