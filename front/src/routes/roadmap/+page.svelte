977b2f66a012793c2054646237089214809fcbcd
Nicolas Sauvage <noki.sauvage@gmail.com>
2025-09-09 12:15:05 +0200

    update changelog and roadmap

--- front/src/routes/roadmap/+page.svelte
+++ front/src/routes/roadmap/+page.svelte
@@ -1,13 +1,11 @@
 <script>
 	const roadmap = [
-		'display the list of steps on top of stepper instead of 1/4...',
 		'add emoji reactions to the cards',
 		'hide logout link when user is not logged in',
 		'order tickets by number of votes',
 		'add a timer for the write and vote pages',
 		'improve confetti',
-		'limit the number of votes per user',
-		'allow choosing the name of the columns'
+		'limit the number of votes per user'
 	];
 </script>
 
